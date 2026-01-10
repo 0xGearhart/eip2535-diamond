91b4da986bb1c820892615b05f57143e4edb4f71
gearhart <geardev26@gmail.com>
2026-01-09 20:39:21 -0800

    add deployment script outline

---
+++ script/DeployDIamond.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+import {CodeConstants, HelperConfig} from "./HelperConfig.s.sol";
+import {Script} from "forge-std/Script.sol";
+
+pragma solidity ^0.8.27;
+
+contract DeployDiamond is Script, CodeConstants {
+    function run() external {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getNetworkConfig();
+
+        vm.startBroadcast(config.account);
+
+        vm.stopBroadcast();
+    }
+}

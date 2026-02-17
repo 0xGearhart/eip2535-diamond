42f180b5058024725349d70b1b9f108d822783fa
0xGearhart <geardev26@gmail.com>
2026-02-16 18:00:01 -0800

    add owner is address(0) check and renounce ownership function

--- src/facets/OwnershipFacet.sol
+++ src/facets/OwnershipFacet.sol
@@ -5,12 +5,22 @@ import {IERC173} from "src/interfaces/IERC173.sol";
 import {LibDiamond} from "src/libraries/LibDiamond.sol";
 
 contract OwnershipFacet is IERC173 {
+    error OwnershipFacet__NewOwnerIsZeroAddress();
+
     function owner() external view override returns (address owner_) {
         owner_ = LibDiamond.contractOwner();
     }
 
     function transferOwnership(address _newOwner) external override {
         LibDiamond.enforceIsContractOwner();
+        if (_newOwner == address(0)) {
+            revert OwnershipFacet__NewOwnerIsZeroAddress();
+        }
         LibDiamond.setContractOwner(_newOwner);
+    }
+
+    function renounceOwnership() external {
+        LibDiamond.enforceIsContractOwner();
+        LibDiamond.setContractOwner(address(0));
     }
 }

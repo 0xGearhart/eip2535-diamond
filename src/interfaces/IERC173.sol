906c4d919f74c281f1eca3b20e79712fdfa83a3b
0xGearhart <geardev26@gmail.com>
2026-03-02 10:06:29 -0800

    add Natspec documentation

--- src/interfaces/IERC173.sol
+++ src/interfaces/IERC173.sol
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.33;
 
+/// @title ERC-173 Contract Ownership Interface
+/// @author 0xGearhart
+/// @notice Standard interface for basic ownership in contracts.
 interface IERC173 {
+    /// @notice Emitted when ownership is transferred.
+    /// @param previousOwner Previous owner address.
+    /// @param newOwner New owner address.
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
+    /// @notice Returns the current owner.
+    /// @return owner_ Current owner address.
     function owner() external view returns (address owner_);
 
+    /// @notice Transfers ownership to a new owner.
+    /// @param _newOwner Address to receive ownership.
     function transferOwnership(address _newOwner) external;
 }
